279320b9929f49a5600e0aed055dcb97454e7d3b
Muhammad Taaha <taaha.waseem@invozone.com>
2022-11-02 18:02:55 +0500

    Added Hello World route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[get("/hello/{name}")]
+async fn greet(req:actix_web::HttpRequest) -> impl Responder {
+    format!("Hello ")
+}
+
+#[actix_web::main] // or #[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/hello", web::get().to(|| async { "Hello World!" }))
+            .service(greet)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
