c10924a9dd8f304878c69c4edc540c5819aa5420
Muhammad Taaha <taaha.waseem@invozone.com>
2022-11-03 13:43:06 +0500

    feat: add Subscribe end point and related tests

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,15 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use std::net::TcpListener;
 
-#[get("/hello/{name}")]
-async fn greet(req:actix_web::HttpRequest) -> impl Responder {
-    format!("Hello ")
-}
+use zero2prod::run;
 
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/hello", web::get().to(|| async { "Hello World!" }))
-            .service(greet)
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    let listener = TcpListener::bind("127.0.0.1:8080").expect("Failed to bind address");
+    run(listener)?.await
+}
+
+
+#[cfg(test)]
+mod tests{
+    // Here we would write internal tests for private sub routines 
 }
