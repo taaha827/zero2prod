7e866f2caf321540343ad8a95c1969810ecc82be
Muhammad Taaha <taaha.waseem@invozone.com>
2022-11-03 17:30:44 +0500

    Docs: Add expalnatory comment(s)

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,16 @@
 use std::net::TcpListener;
-
+// Rather then importing everything individually, RUST allows to  import multiple items from a crack using to some syntax 
+// Like how jave script dose it that is the Curly BRacket Notation
 use actix_web::{get, web, App, HttpServer, HttpResponse,Responder};
 use actix_web::dev::Server;
+
+// This is an example of attribute like macro
 #[get("/hello/{name}")]
+// This is an example in actix_web to extract data from the request, directly
+// Actix web allows upto 10 items here, there are other options like FORM, PATH,QUERY,JSON
 async fn greet(name: web::Path<String>) -> impl Responder {
+    // Another Example of a simple macro that basically matches the attributes passed into it and produces code accordingly
+    // #metaprogramming
     format!("Hello {name}!",)
 }
 async fn health_check() -> impl Responder {
@@ -11,17 +18,16 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
+// This is an example of a method like macro, that will use the  struct given to it and will
+// Create serialize and deserialize attributes from it.
 #[derive(serde::Deserialize)]
 struct FormData{
     email:String,
     name:String
 }
-
-
 async fn subscribe(_form:web::Form<FormData>) -> impl Responder {
     HttpResponse::Ok()
 }
-
 pub fn run(listner: TcpListener) -> Result<Server,std::io::Error> {
     let server  = HttpServer::new(|| {
         App::new()
@@ -30,7 +36,7 @@ pub fn run(listner: TcpListener) -> Result<Server,std::io::Error> {
             .route("/hello", web::get().to(|| async { "Hello World!" }))
             .service(greet)
     })
-    .listen(listner)?
+    .listen(listner)? // Thats why there is a `?` here if the listen fails to bind to the listener the run function is not called, and the error is thrown one level up the call stack
     .run();
     Ok(server)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
+// Importing the Tcp Listnerer Structure
 use std::net::TcpListener;
 
 use zero2prod::run;
 
-#[actix_web::main] // or #[tokio::main]
+
+
+// Using Declarative macro to technically create a main function that will run asynchrnously, as by default you can not have the main function
+// Of a rust service set as async
+#[actix_web::main]
+/* 
+Returning A Result Data type, remember that by default RUST treat lines of code as expression (i.e: Lines that perform an action and return a value, and the last line of a function is always implied as return statement)
+*/ 
+// Result<()> is used so that it shows that it will return OK signal without any value
+// Or error if needed
 async fn main() -> std::io::Result<()> {
+    // Bind returns a RESULT, so if we want to cater that we use expect, which will cause the code to exit if the bind function returns an error
+    // Expect is not like catch just a way to catch an error and modify the error message, it does not allow to write a function per se.
     let listener = TcpListener::bind("127.0.0.1:8080").expect("Failed to bind address");
+    // ? allows to proceed only if there is no error, other wise throw the error back to who ever has called the function
     run(listener)?.await
 }
 
